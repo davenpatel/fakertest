31f8afb8fb6b8150eaf9c91a424ed2f5e3bbaa87
davenpatel <davenptl@gmail.com>
2026-05-25 02:22:54 +0000

    Update dod_id generation.

--- main.py
+++ main.py
@@ -11,27 +11,34 @@ from src.models import Address, Personnel
 def initialize_random_seed(seed: int = 0) -> None:
     Faker.seed(seed)
     random.seed(seed)
+    
+def generate_dod_ids(count: int) -> list[int]:
+    faker = Faker()
+    dod_ids = set()
 
-def generate_personnel_records(count: int) -> list[Personnel]:
+    while len(dod_ids) < count:
+        dod_id = faker.unique.ssn().replace('-', '')
+        dod_ids.add(dod_id)
+
+    return list(dod_ids)
+
+def generate_personnel_records(dod_ids: list[int]) -> list[Personnel]:
     faker = Faker()
     personnel = []
 
-    for _ in range(count):
-        dod_id = faker.unique.ssn().replace('-', '')
-        personnel.append(Personnel(dod_id))
+    for id in dod_ids:
+        personnel.append(Personnel(id))
 
     return personnel
 
-def generate_address_records(count: int) -> list[Address]:
+def generate_address_records(dod_ids: list[int]) -> list[Address]:
     faker = Faker()
     addresses = []
 
-    for _ in range(count):
-        dod_id = faker.unique.ssn().replace('-', '')
-        addresses.append(Address(dod_id))
+    for id in dod_ids:
+        addresses.append(Address(id))
 
     return addresses
-
 
 def write_csv(filename: str, records: list, record_type: type) -> None:
     with open(filename, "w", encoding="utf-8") as f:
@@ -41,8 +48,9 @@ def write_csv(filename: str, records: list, record_type: type) -> None:
 def main() -> None:
     initialize_random_seed()
 
-    personnel = generate_personnel_records(100000)
-    addresses = generate_address_records(100000)
+    dod_ids = generate_dod_ids(100000)
+    personnel = generate_personnel_records(dod_ids)
+    addresses = generate_address_records(dod_ids)
 
     write_csv("users.csv", personnel, Personnel)
     write_csv("addresses.csv", addresses, Address)
